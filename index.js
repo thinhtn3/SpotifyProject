56c9880824ad7280630173fe1c48034a83d05f61
thinhtn3 <thinhttnguyen@gmail.com>
2024-01-21 01:33:06 -0800

    console logged the track and artist names

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.listen(port, () => {
 
 app.get('/login', (req, res) => {
     const state = randomstring.generate(16);
-    const scope = 'user-read-private%20user-read-email%20user-top-read';
+    const scope = 'user-read-private%20user-read-email%20user-top-read%20user-read-playback-state%20user-read-currently-playing';
     res.redirect(`https://accounts.spotify.com/authorize?client_id=102fca1d546c40c99647f9ce7c69a203&response_type=code&redirect_uri=http://localhost:8080/callback&scope=${scope}&state=${state}`);
 });
 
-app.get('/callback' , (req, res) => {
-    var params =  new URLSearchParams();
+app.get('/callback', (req, res) => {
+    var params = new URLSearchParams();
     params.append('grant_type', 'authorization_code');
     params.append('code', req.query.code);
     params.append('redirect_uri', 'http://localhost:8080/callback');
@@ -31,14 +31,42 @@ app.get('/callback' , (req, res) => {
     //     'redirect_uri': 'http://localhost:8080/callback'
     // };
     const header = {
-        'Content-Type':'application/x-www-form-urlencoded',
-		'Authorization': 'Basic ' + (new Buffer.from(my_client_id + ':' + my_client_secret).toString('base64'))
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer.from(my_client_id + ':' + my_client_secret).toString('base64'))
     }
-    fetch('https://accounts.spotify.com/api/token', {method: 'POST', body: params, headers: header})
-        .then(response=>response.json())
+    fetch('https://accounts.spotify.com/api/token', { method: 'POST', body: params, headers: header })
+        .then(response => response.json())
         .then(data => {
             accessToken = data.access_token;
             refreshToken = data.refresh_token;
-            res.send(accessToken)
+            res.redirect('http://localhost:8080/getTopTracks')
+        })
+})
+
+app.get('/getTopTracks', (req, res) => {
+    const accessHeader = {
+        Authorization: 'Bearer ' + accessToken
+    }
+    fetch('https://api.spotify.com/v1/me/top/tracks', { method: 'GET', headers: accessHeader })
+        .then(response => response.json())
+        .then(data => {
+            let topTrackArray = [];
+            let trackRes = []
+            for (tracks of data.items) {
+                let topTrackArtistArr = [];
+                let trackObject = {};
+                trackObject.trackName = tracks.name;
+
+                for (let i = 0; i < tracks.artists.length; i++) {
+                    topTrackArtistArr.push(tracks.artists[i].name);
+                }
+                trackObject.artistNames = topTrackArtistArr;
+                topTrackArray.push(trackObject);
+            }
+            
+            for (tracks of topTrackArray) {
+                console.log(tracks);
+            }
+            res.send(topTrackArray)
         })
 })
